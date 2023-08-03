b13216f8d2901eeb3779abab18faf992a2629f5b
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-03 23:13:50 +0600

    git commit

--- task.js
+++ task.js
@@ -8,6 +8,34 @@
 // avrg(10,20,30,40)
 // console.log(avrg(10,20,30,40,50));
 // problem 3
-var items =[10,20,30,40]
-const sum = items.reduce((a, b) => a + b, 0)
-console.log(sum / 4);
+// var items =[10,20,30,40]
+// const sum = items.reduce((a, b) => a + b, 0)
+// console.log(sum / 4);
+
+// var color = "red"
+// switch(color){
+//     case "green":
+//     console.log("you are my best friend");
+//     break;
+//     case "white":
+//     console.log("you are my extra frind");
+//     break;
+//     default:console.log("your are my hunki friend");
+// }
+// var signal ="Green"
+// switch(signal){
+//     case "red":
+//         console.log("Yor are in denger");
+//         break
+//         case "yello":
+//         console.log("you should STOP");
+//         break
+//         case "Green":
+//             console.log("you Should cross The road");
+//             break
+//             default: "none"
+// }
+
+// function show(){
+//     console.log("hello world");
+// }
